9d95b0aaf78727acbe14ba647a1f6f8c36cac34e
amnonih <129008477+amnonih@users.noreply.github.com>
2023-05-16 01:22:57 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("api/", include("weather_api.urls")),
+    path('current/', views.current_weather, name='current_weather'),
+    path('past/', views.past_weather, name='past_weather'),
+    path('forecast/', views.forecast_weather, name='forecast_weather'),
 ]
